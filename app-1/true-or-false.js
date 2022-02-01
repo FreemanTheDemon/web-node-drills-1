6b59db74d7a16c5c1f348096797ab44c5f249bee
Freeman Bird <freemanbird@gmail.com>
2022-02-01 11:12:56 -0700

    completed app-1

---
+++ app-1/true-or-false.js
@@ -0,0 +1,9 @@
+function simple() {
+}
+function simple() {console.log('Simple')}
+simple()
+let array = [[],[{age: 21, color: "blue"}]]
+if(array[0]===[]){ console.log("It's truthy!")}else{console.log("False")}
+if(array[0]==[]){ console.log("It's truthy!")}else{console.log("False")}
+if(array[0]=[]){ console.log("It's truthy!")}else{console.log("False")}
+array="green"
